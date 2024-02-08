e7d9796371c0b3b8d8da79fea18270b2867fecd7
usilgtfoms <usilgtfoms@irkoms.ru>
2024-02-08 15:18:50 +0800

    Added login method in identity controller

---
+++ Web/Controllers/IdentityController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Models;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public IActionResult Login(AuthenticateRequest model, IIdentityService userService)
+        {
+            var answer = userService.Authenticate(model);
+            if (answer == null)
+                return Unauthorized();
+            return Ok(answer);
+        }
+    }
+}
