a0ea0d3715a903aad8e474e8b17e3ec104072f8d
usilgtfoms <usilgtfoms@irkoms.ru>
2024-02-15 16:53:28 +0800

    Login form

--- Web/Client/persons-vue/src/App.vue
+++ Web/Client/persons-vue/src/App.vue
@@ -1,19 +1,93 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { Locked, User } from "@vicons/carbon";
+import { NCard, NIcon, NForm, NFormItem, NInput, NButton } from "naive-ui";
+
+var model = {
+  email: null,
+  password: null,
+};
+var loading = false;
+var rules = {};
+var login = false;
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="login">
+    <n-card>
+      <h2>Вход в систему</h2>
+      <n-form class="login-form" :model="model" :rules="rules" ref="form">
+        <n-form-item prop="username">
+          <n-input
+            v-model="model.email"
+            placeholder="Email"
+            type="email"
+            :prefix-icon="User"
+          >
+            <template #prefix>
+              <n-icon :component="User"></n-icon>
+            </template>
+          </n-input>
+        </n-form-item>
+        <n-form-item prop="password">
+          <n-input v-model="model.password" placeholder="Password" type="password">
+            <template #prefix>
+              <n-icon :component="Locked"></n-icon>
+            </template>
+          </n-input>
+        </n-form-item>
+        <n-form-item>
+          <n-button
+            :loading="loading"
+            class="login-button"
+            type="primary"
+            native-type="submit"
+            block
+            >Войти</n-button
+          >
+        </n-form-item>
+        <a class="forgot-password" href="#">Забыли пароль?</a>
+      </n-form>
+    </n-card>
 
+    <!--    <n-card>
+      <h2>Вход в систему</h2>
+      <n-form
+        class="login-form"
+        :model="model"
+        :rules="rules"
+        ref="form"
+        @submit.native.prevent="login"
+      >
+        <n-form-item prop="username">
+          <n-input
+            v-model="model.email"
+            placeholder="Email"
+            type="email"
+            :prefix-icon="User"
+          ></n-input>
+        </n-form-item>
+        <n-form-item prop="password">
+          <n-input
+            v-model="model.password"
+            placeholder="Password"
+            type="password"
+            :prefix-icon="Lock"
+          ></n-input>
+        </n-form-item>
+        <n-form-item>
+          <n-button
+            :loading="loading"
+            class="login-button"
+            type="primary"
+            native-type="submit"
+            block
+          >Войти</n-button>
+        </n-form-item>
+        <a class="forgot-password" href="#">Забыли пароль?</a>
+      </n-form>
+    </n-card>-->
+  </div>
+</template>
 <style scoped>
 .logo {
   height: 6em;
---
+++ Web/Client/persons-vue/src/Utils/axios.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import router from "../router/index";
+
+const api = axios.create();
+
+//start request
+api.interceptors.request.use(
+  (config) => {
+    if (localStorage.access_token) {
+      config.headers.authorization = `Bearer ${localStorage.access_token}`;
+    }
+    //Надо возвращать конфиг после его модификации
+    return config;
+  },
+  (error) => {
+    //Этот блок кода срабатывает только тогда, когда ошибка отправки запроса с фронта
+    console.log(error);
+  }
+);
+//end request
+
+//start response
+api.interceptors.response.use(
+  (config) => {
+    if (localStorage.access_token) {
+      config.headers.authorization = `Bearer ${localStorage.access_token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    //Этот блок кода срабатывает когда прилетает ошибка с бэка
+
+    if (error.response.data.message === "Token has expired") {
+      axios
+        .post(
+          "api/auth/refresh",
+          {},
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.access_token}`,
+            },
+          }
+        )
+        .then((response) => {
+          localStorage.access_token = response.data.access_token;
+          //Делаем повторный запрос на получение данных с новым токеном
+          //чтобы вручную не обновлять страницу
+          error.config.headers.authorization = `Bearer ${response.data.access_token}`;
+
+          return api.request(error.config);
+        });
+    }
+
+    if (error.response.status === 401) {
+      router.push("/login");
+    }
+  }
+);
+export default api;
