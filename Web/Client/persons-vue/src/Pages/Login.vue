c766de880cb864e855ff71feb82f28d0e0a91aa7
usilgtfoms <usilgtfoms@irkoms.ru>
2024-02-15 16:57:58 +0800

    Moved login form to a separate file

---
+++ Web/Client/persons-vue/src/Pages/Login.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { Locked, User } from "@vicons/carbon";
+import { NCard, NIcon, NForm, NFormItem, NInput, NButton } from "naive-ui";
+
+var model = {
+  email: null,
+  password: null,
+};
+var loading = false;
+var rules = {};
+var login = false;
+</script>
+
+<template>
+  <div class="login">
+    <n-card>
+      <h2>Вход в систему</h2>
+      <n-form class="login-form" :model="model" :rules="rules" ref="form">
+        <n-form-item prop="username">
+          <n-input
+            v-model="model.email"
+            placeholder="Email"
+            type="email"
+            :prefix-icon="User"
+          >
+            <template #prefix>
+              <n-icon :component="User"></n-icon>
+            </template>
+          </n-input>
+        </n-form-item>
+        <n-form-item prop="password">
+          <n-input v-model="model.password" placeholder="Password" type="password">
+            <template #prefix>
+              <n-icon :component="Locked"></n-icon>
+            </template>
+          </n-input>
+        </n-form-item>
+        <n-form-item>
+          <n-button
+            :loading="loading"
+            class="login-button"
+            type="primary"
+            native-type="submit"
+            block
+            >Войти</n-button
+          >
+        </n-form-item>
+        <a class="forgot-password" href="#">Забыли пароль?</a>
+      </n-form>
+    </n-card>
+  </div>
+</template>
--- Web/Client/persons-vue/src/main.js
+++ Web/Client/persons-vue/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import Login from './Pages/Login.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(Login).mount('#app')
