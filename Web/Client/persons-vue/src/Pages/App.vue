bd187e3c1b60a5f438bdc92272d0a0bf9a785709
usilgtfoms <usilgtfoms@irkoms.ru>
2024-05-21 16:33:00 +0800

    Added menu components

--- Web/Client/persons-vue/src/Pages/App.vue
+++ Web/Client/persons-vue/src/Pages/App.vue
@@ -1,125 +1,17 @@
 <script setup>
-import { h, defineComponent, ref } from "vue";
-import {
-  NLayout,
-  NLayoutHeader,
-  NLayoutFooter,
-  NSpace,
-  NMenu,
-  NLayoutSider,
-} from "naive-ui";
-import { NIcon } from "naive-ui";
-import {
-  BookOutline as BookIcon,
-  PersonOutline as PersonIcon,
-  WineOutline as WineIcon,
-} from "@vicons/ionicons5";
-
-function renderIcon(icon) {
-  return () => h(NIcon, null, { default: () => h(icon) });
-}
-
-const menuOptions = [
-  {
-    label: "Hear the Wind Sing",
-    key: "hear-the-wind-sing",
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: "Pinball 1973",
-    key: "pinball-1973",
-    icon: renderIcon(BookIcon),
-    disabled: true,
-    children: [
-      {
-        label: "Rat",
-        key: "rat",
-      },
-    ],
-  },
-  {
-    label: "A Wild Sheep Chase",
-    key: "a-wild-sheep-chase",
-    disabled: true,
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: "Dance Dance Dance",
-    key: "Dance Dance Dance",
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        type: "group",
-        label: "People",
-        key: "people",
-        children: [
-          {
-            label: "Narrator",
-            key: "narrator",
-            icon: renderIcon(PersonIcon),
-          },
-          {
-            label: "Sheep Man",
-            key: "sheep-man",
-            icon: renderIcon(PersonIcon),
-          },
-        ],
-      },
-      {
-        label: "Beverage",
-        key: "beverage",
-        icon: renderIcon(WineIcon),
-        children: [
-          {
-            label: "Whisky",
-            key: "whisky",
-          },
-        ],
-      },
-      {
-        label: "Food",
-        key: "food",
-        children: [
-          {
-            label: "Sandwich",
-            key: "sandwich",
-          },
-        ],
-      },
-      {
-        label: "The past increases. The future recedes.",
-        key: "the-past-increases-the-future-recedes",
-      },
-    ],
-  },
-];
+import VerticalMenu from "../components/menu/VerticalMenu.vue";
+import HorizontalMenu from "../components/menu/HorizontalMenu.vue";
+import { NLayout } from "naive-ui";
 </script>
 
 <template>
   <div class="main">
-    <n-space vertical>
-      <n-layout style="height: calc(100vh-20px)">
-        <n-layout-header bordered>
-          <n-menu mode="horizontal" :options="menuOptions" />
-        </n-layout-header>
-        <n-layout has-sider style="height: 100%">
-          <n-layout-sider
-            bordered
-            show-trigger
-            collapse-mode="width"
-            :collapsed-width="64"
-            :width="240"
-            :native-scrollbar="false"
-          >
-            <n-menu
-              :collapsed-width="64"
-              :collapsed-icon-size="22"
-              :options="menuOptions"
-            />
-          </n-layout-sider>
-        </n-layout>
+    <n-layout>
+      <HorizontalMenu></HorizontalMenu>
+      <n-layout has-sider style="height: 95%">
+        <VerticalMenu></VerticalMenu>
       </n-layout>
-    </n-space>
+    </n-layout>
   </div>
 </template>
 <style>
@@ -128,8 +20,8 @@ const menuOptions = [
   background: rgba(128, 128, 128, 0.2);
 }
 div.main {
-  /* width: 100%;
-  height: 100vh;*/
+  height: 100vh;
+  display: flex;
 }
 
 .n-layout-sider {
---
+++ Web/Client/persons-vue/src/components/menu/VerticalMenu.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { h } from "vue";
+import { NIcon, NMenu, NLayoutSider } from "naive-ui";
+import {
+  BookOutline as BookIcon,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon,
+} from "@vicons/ionicons5";
+
+function renderIcon(icon) {
+  return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+const leftMenuOptions = [
+  {
+    label: "Диспансеризация",
+    key: "disp",
+    icon: renderIcon(BookIcon),
+  },
+];
+</script>
+<template>
+  <n-layout-sider
+    bordered
+    show-trigger
+    collapse-mode="width"
+    :collapsed-width="64"
+    :width="240"
+    :native-scrollbar="false"
+  >
+    <n-menu :collapsed-width="64" :collapsed-icon-size="22" :options="leftMenuOptions" />
+  </n-layout-sider>
+</template>
+<style></style>
