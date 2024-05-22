f97a5e46089b12f1c4d04516d09db2f97ef1f524
usilgtfoms <usilgtfoms@irkoms.ru>
2024-05-22 17:54:51 +0800

    Added tables table

--- Web/Client/persons-vue/src/Pages/Spiski.vue
+++ Web/Client/persons-vue/src/Pages/Spiski.vue
@@ -1,17 +1,52 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+import { NTable, NTabs, NTabPane } from "naive-ui";
 import Layout from "../components/layouts/Layout.vue";
 import { useTablesStore } from "../stores/tablesStore";
 
 const tablesStore = useTablesStore();
+var tables = ref([]);
+
 onMounted(() => {
-  tablesStore.getTables().then(response=>{console.log(response)});
+  tablesStore.getTables().then((response) => {
+    tables.value = response;
+    console.log(tables);
+  });
 });
 </script>
 
 <template>
-    <router-view> </router-view>
-    <Layout>
-    !!!!!!!!!!!!!
-    </Layout>
+  <router-view> </router-view>
+  <Layout>
+    <div class="content">
+      <n-tabs type="segment" animated>
+        <n-tab-pane name="Активные" tab="Active">
+          <n-table>
+            <thead>
+              <tr>
+                <th>Наименование</th>
+                <th>Описание</th>
+                <th>Дата создания</th>
+                <th>Дата закрытия</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="table in tables">
+                <td>{{ table.name }}</td>
+                <td>{{ table.description }}</td>
+                <td>{{ table.createdAt }}</td>
+                <td>{{ table.expiredAt }}</td>
+              </tr>
+            </tbody>
+          </n-table>
+        </n-tab-pane>
+        <n-tab-pane name="Закрытые" tab="Closed"> </n-tab-pane>
+      </n-tabs>
+    </div>
+  </Layout>
 </template>
+<style>
+.content {
+  padding: 20px;
+}
+</style>
