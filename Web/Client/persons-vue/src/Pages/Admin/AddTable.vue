60e23c4413024373ef4267c77bd160c40bad0ee5
usilgtfoms <usilgtfoms@irkoms.ru>
2024-06-06 13:32:57 +0800

    refactor: move table form in separated component

--- Web/Client/persons-vue/src/Pages/Admin/AddTable.vue
+++ Web/Client/persons-vue/src/Pages/Admin/AddTable.vue
@@ -1,100 +1,20 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import Layout from "../../components/layouts/Layout.vue";
-import { NForm, NFormItem, NSpace, NButton, NInput, NGrid, NGridItem } from "naive-ui";
+import FormTable from "../../components/table/FormTable.vue";
 import { useTablesStore } from "../../stores/tablesStore";
-import { useFormTableValidation } from "../../validations/tableValidation";
 
-const tablesStore = useTablesStore();
 var formRef = ref(null);
-var buttonSaveIsLoading = ref(false);
 
-const { rules } = useFormTableValidation(tablesStore.table);
-
-function handleSaveButtonClick(e) {
-  tablesStore.table.notFoundTable = false;
-  e.preventDefault();
-  formRef.value?.validate((errors) => {
-    if (!errors) {
-      buttonSaveIsLoading.value = true;
-      tablesStore
-        .CheckExistTable(
-          tablesStore.table.database,
-          tablesStore.table.shema,
-          tablesStore.table.tableName
-        )
-        .then((response) => {
-          console.log(response); //TODO
-          buttonSaveIsLoading.value = false;
-        })
-        .catch((error) => {
-          tablesStore.table.notFoundTable = true;
-          formRef.value?.validate();
-        })
-        .finally(() => {
-          buttonSaveIsLoading.value = false;
-        });
-    } else {
-      console.log(errors);
-      message.error("Invalid");
-    }
-  });
-}
-
-onMounted(() => {
-  /*  tablesStore.loadTable().then((response) => {
-    tables.value = response;
-    console.log(tables);
-  });*/
-});
+onMounted(() => {});
 </script>
 
 <template>
   <router-view> </router-view>
   <Layout>
     <div class="content">
-      <n-form
-        ref="formRef"
-        size="small"
-        :model="tablesStore.table"
-        :rules="rules"
-        label-placement="left"
-        require-mark-placement="right-hanging"
-        :style="{
-          maxWidth: '640px',
-        }"
-      >
-        <n-form-item label="Название" path="name">
-          <n-input v-model:value="tablesStore.table.name" placeholder="" />
-        </n-form-item>
-        <n-form-item :show-feedback="false">
-          <n-space>
-            <n-form-item label="База данных" label-placement="top" path="database">
-              <n-input v-model:value="tablesStore.table.database" placeholder="" />
-            </n-form-item>
-            <n-form-item label="Схема" label-placement="top" path="shema">
-              <n-input v-model:value="tablesStore.table.shema" placeholder="" />
-            </n-form-item>
-            <n-form-item label="Таблица" label-placement="top" path="tableName">
-              <n-input v-model:value="tablesStore.table.tableName" placeholder="" />
-            </n-form-item>
-          </n-space>
-        </n-form-item>
-        <n-form-item label="Описание">
-          <n-input v-model:value="tablesStore.table.description" placeholder="" />
-        </n-form-item>
-
-        <div style="display: flex; justify-content: flex-end">
-          <n-button
-            :loading="buttonSaveIsLoading"
-            round
-            type="primary"
-            @click="handleSaveButtonClick"
-          >
-            Проверить
-          </n-button>
-        </div>
-      </n-form>
+      Добавление таблицы
+      <form-table></form-table>
     </div>
   </Layout>
 </template>
---
+++ Web/Client/persons-vue/src/components/table/FormTable.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { ref } from "vue";
+import { NForm, NFormItem, NSpace, NButton, NInput } from "naive-ui";
+import { useTablesStore } from "../../stores/tablesStore";
+import { useFormTableValidation } from "../../validations/tableValidation";
+
+const tablesStore = useTablesStore();
+var formRef = ref(null);
+var buttonSaveIsLoading = ref(false);
+
+const { rules } = useFormTableValidation(tablesStore.table);
+
+function handleSaveButtonClick(e) {
+  tablesStore.table.notFoundTable = false;
+  e.preventDefault();
+  formRef.value?.validate((errors) => {
+    if (!errors) {
+      buttonSaveIsLoading.value = true;
+      tablesStore
+        .CheckExistTable(
+          tablesStore.table.database,
+          tablesStore.table.shema,
+          tablesStore.table.tableName
+        )
+        .then((response) => {
+          console.log(response); //TODO
+          buttonSaveIsLoading.value = false;
+        })
+        .catch((error) => {
+          tablesStore.table.notFoundTable = true;
+          formRef.value?.validate();
+        })
+        .finally(() => {
+          buttonSaveIsLoading.value = false;
+        });
+    } else {
+      console.log(errors);
+      message.error("Invalid");
+    }
+  });
+}
+</script>
+
+<template>
+  <n-form
+    ref="formRef"
+    size="small"
+    :model="tablesStore.table"
+    :rules="rules"
+    label-placement="left"
+    require-mark-placement="right-hanging"
+    :style="{
+      maxWidth: '640px',
+    }"
+  >
+    <n-form-item label="Название" path="name">
+      <n-input v-model:value="tablesStore.table.name" placeholder="" />
+    </n-form-item>
+    <n-form-item :show-feedback="false">
+      <n-space>
+        <n-form-item label="База данных" label-placement="top" path="database">
+          <n-input v-model:value="tablesStore.table.database" placeholder="" />
+        </n-form-item>
+        <n-form-item label="Схема" label-placement="top" path="shema">
+          <n-input v-model:value="tablesStore.table.shema" placeholder="" />
+        </n-form-item>
+        <n-form-item label="Таблица" label-placement="top" path="tableName">
+          <n-input v-model:value="tablesStore.table.tableName" placeholder="" />
+        </n-form-item>
+      </n-space>
+    </n-form-item>
+    <n-form-item label="Описание">
+      <n-input v-model:value="tablesStore.table.description" placeholder="" />
+    </n-form-item>
+
+    <div style="display: flex; justify-content: flex-end">
+      <n-button
+        :loading="buttonSaveIsLoading"
+        round
+        type="primary"
+        @click="handleSaveButtonClick"
+      >
+        Проверить
+      </n-button>
+    </div>
+  </n-form>
+</template>
+<style></style>
